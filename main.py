8f90cf11783900396a3ad8fb02172f707082de58
samad-xd <samad.xd@icloud.com>
2026-06-24 19:04:03 +0530

    use asyncio

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
-import socket
-import threading
+import asyncio
 
 
-def handle_client(conn, addr):
+async def handle_client(reader, writer):
+    addr = writer.get_extra_info("peername")
     print(f"Connected to {addr}")
 
     try:
         while True:
-            data = conn.recv(1024)
+            data = await reader.read(1024)
 
             if not data:
                 print(f"Client {addr} connection closed")
@@ -17,30 +17,22 @@ def handle_client(conn, addr):
         print(f"Connection {addr} forcibly disconnected")
 
     finally:
-        conn.close()
+        writer.close()
+        await writer.wait_closed()
 
 
-def main():
-    server = socket.create_server(address=("localhost", 6379))
-    server.settimeout(1)
+async def main():
+    server = await asyncio.start_server(
+        client_connected_cb=handle_client, host="localhost", port=6379
+    )
     print("Server Started")
 
-    try:
-        while True:
-            try:
-                conn, addr = server.accept()
-
-            except socket.timeout:
-                continue
-
-            threading.Thread(target=handle_client, args=(conn, addr), daemon=True).start()
-
-    except KeyboardInterrupt:
-        print("Shutting down server")
-
-    finally:
-        server.close()
+    async with server:
+        await server.serve_forever()
 
 
 if __name__ == "__main__":
-    main()
+    try:
+        asyncio.run(main())
+    except KeyboardInterrupt:
+        print("Shutting down server")
